d0274531e662c1d9451911527cde3df4f7c42192
andresc2003 <cedenoandres18@gmail.com>
2023-02-06 20:47:55 -0600

    fix: Semana 4

---
+++ src/main/java/com/tienda_l/controller/ClienteController.java
@@ -0,0 +1,31 @@
+
+package com.tienda_l.controller;
+
+import com.tienda_l.domain.Cliente;
+import dao.ClienteDao;
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class ClienteController {
+    
+    @Autowired
+    private ClienteDao clienteDao;
+            
+    @GetMapping("/")
+    public String inicio(Model model) {
+        var variable="Hola desde el Back-end";
+        model.addAttribute("mensaje", variable);
+        
+        
+        var clientes = clienteDao.findAll();
+        model.addAttribute("clientes",clientes);
+        return "index";
+    }
+    
+}
---
+++ src/main/java/dao/ClienteDao.java
@@ -0,0 +1,11 @@
+
+package dao;
+
+import com.tienda_l.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface ClienteDao extends CrudRepository<Cliente, Long> {
+    
+}
