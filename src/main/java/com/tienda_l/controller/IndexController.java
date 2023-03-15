8fa0126b795c630a2da3bd32600656596f42f97c
andresc2003 <cedenoandres18@gmail.com>
2023-03-15 16:50:21 -0600

    fix: semana 9

--- src/main/java/com/tienda_l/controller/IndexController.java
+++ src/main/java/com/tienda_l/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.tienda_l.controller;
 
 import com.tienda_l.domain.Cliente;
 import com.tienda_l.dao.ClienteDao;
+import com.tienda_l.service.ArticuloService;
 import com.tienda_l.service.ClienteService;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
@@ -12,15 +13,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@Service
 @Controller
 @Slf4j
 public class IndexController {
+    
+    @Autowired
+    private ArticuloService articuloService;
 
     @GetMapping("/")
-    public String inicio(Model model) {
+public String inicio(Model model) {
+        var articulos = articuloService.getArticulos(true);
+        model.addAttribute("articulos", articulos);
 
-        return "index";
+        return "/index";
     }
 
 }
--- src/main/java/com/tienda_l/domain/Cliente.java
+++ src/main/java/com/tienda_l/domain/Cliente.java
@@ -1,37 +1,40 @@
-
 package com.tienda_l.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
 import lombok.Data;
 
- @Data
- @Entity
- @Table(name="cliente")
- 
-public class Cliente implements Serializable{
-     
+@Data
+@Entity
+@Table(name = "cliente")
+
+public class Cliente implements Serializable {
+
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idCliente;
-    
+
     private String nombre;
     private String apellidos;
     private String correo;
     private String telefono;
-    
-    public Cliente(){
-        
+
+    @JoinColumn(name = "id_credito", referencedColumnName = "id_credito")
+    @ManyToOne
+    private Credito credito;
+
+    public Cliente() {
+
     }
-    
-    public Cliente(String nombre, String apellidos, String correo, String telefono) {
+
+    public Cliente(String nombre, String apellidos, String correo, String telefono, Credito credito) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.correo = correo;
         this.telefono = telefono;
+        this.credito = credito;
     }
-    
-    
+
 }
