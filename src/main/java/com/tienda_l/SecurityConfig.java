da69897339fa030c33d6c3fccd52138b8cc05ce1
andresc2003 <cedenoandres18@gmail.com>
2023-03-20 20:33:48 -0600

    fix: Semana 10

---
+++ src/main/java/com/tienda_l/SecurityConfig.java
@@ -0,0 +1,36 @@
+
+package com.tienda_l;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+    
+    @Bean
+    public  UserDetailsService users(){
+       UserDetails admin = User.builder()
+               .username("juan")
+                .password("{noop}123")
+                .roles("USER", "VENDEDOR", "ADMIN")
+                .build();
+        UserDetails sales = User.builder()
+               .username("rebeca")
+                .password("{noop}456")
+                .roles("VENDEDOR", "ADMIN")
+                .build();
+         UserDetails user = User.builder()
+               .username("pedro")
+                .password("{noop}789")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, sales, admin);
+    }   
+       
+}
